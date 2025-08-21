6ff87fb28089fdf912c892ad6e5ad6f6f1236199
zhongtao <ztappleemail@163.com>
2025-08-21 18:03:41 +0800

    feat(style): 优化登录页面样式
    - 添加背景图片和样式- 创建登录框样式
    - 添加全局样式文件
    - 调整 body 背景颜色- 定义浮动、清除浮动和常见间距样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,37 @@
+<template>
+  <div class="bg"></div>
+</template>
+
 <script setup lang="ts">
 
 </script>
 
-<template>
-登陆页面<el-button type="primary">Default</el-button>
-</template>
-
-<style scoped>
-
+<style lang="less" scoped>
+.bg {
+    background-image: url("@/assets/bg.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+    .login {
+        width: 500px;
+        height: 300px;
+        padding: 50px;
+        box-shadow: 0 0 10px 10px #f4f4f4;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        margin-top: -200px;
+        left: 10%;
+        .logo {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 40px;
+            h1 {
+                color: rgb(14, 53, 148);
+            }
+        }
+    }
+}
 </style>
