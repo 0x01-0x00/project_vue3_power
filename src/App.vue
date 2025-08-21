9255ef2a82a542ae387bfe5653716991e273f4e0
zhongtao <ztappleemail@163.com>
2025-08-21 13:10:53 +0800

    build(dependencies): 更新项目依赖并移除示例代码
    -安装 vue-router@4、pinia 和 element-plus
    - 安装 less 作为开发依赖
    - 删除 App.vue 和 HelloWorld.vue 中的示例代码

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  npm install vue-router@4 pinia element-plus
+  project_vue3_power % npm install -D less
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
